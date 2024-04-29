8bf20912d1f692259ef5af592f2b4a36a0824081
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2024-04-28 20:48:49 -0500

    add update feature

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import HomeScreen from "./Screens/HomeScreen";
 import DetailsScreen from "./Screens/DetailsScreen";
+import EditJournalScreen from "./Screens/EditJournalScreen";
 import React, { useState, useEffect } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator , useNavigation} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-const navigation = useNavigation();
 
 export default function App() {
   return (
@@ -14,6 +14,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }}/>
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="EditJournal" component={EditJournalScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
